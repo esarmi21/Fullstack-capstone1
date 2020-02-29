aa112d8e78f08b897996c3f1e806cdc124dcb0f8
jay sarmi <esarmi21@gmail.com>
2020-02-29 11:30:12 -0800

    Updates

---
+++ precache-manifest.7a8995b8c9ad3a970fef6659966d1f23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383adc5b0462068c0f3792d1f7ff1bf6",
+    "url": "/Fullstack-capstone1/index.html"
+  },
+  {
+    "revision": "de8bad44fbb5bf628e46",
+    "url": "/Fullstack-capstone1/static/css/main.af8ff7bc.chunk.css"
+  },
+  {
+    "revision": "30a8e41e46fa31894a21",
+    "url": "/Fullstack-capstone1/static/js/2.017b602d.chunk.js"
+  },
+  {
+    "revision": "bc60939987866ab6ad76618ca79a0fec",
+    "url": "/Fullstack-capstone1/static/js/2.017b602d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8bad44fbb5bf628e46",
+    "url": "/Fullstack-capstone1/static/js/main.54d212b2.chunk.js"
+  },
+  {
+    "revision": "4688e1c9090bcd87b48a",
+    "url": "/Fullstack-capstone1/static/js/runtime-main.a4b77ebc.js"
+  }
+]);
