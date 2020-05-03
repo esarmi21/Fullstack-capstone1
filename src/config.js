8fa97c1448702cf489f444b1cffdaf2650198d3d
jay sarmi <esarmi21@gmail.com>
2020-05-02 19:54:51 -0700

    set up for now for zeit deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     API_ENDPOINT: 
-     "https://pure-wave-40477.herokuapp.com"
+     "https://pure-wave-40477.herokuapp.com",
+     API_KEY: process.env.REACT_APP_API_KEY,
   }
   
--- src/services/AuthApiService.js
+++ src/services/AuthApiService.js
@@ -5,7 +5,6 @@ const AuthApiService = {
   postLogin(credentials) {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
       method: 'POST',
-      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
@@ -21,7 +20,7 @@ const AuthApiService = {
   postUser(user) {
      return fetch(`${config.API_ENDPOINT}/users`, {
        method: 'POST',
-       mode: 'no-cors',
+      
        headers: {
          'Content-Type': 'application/json',
          'Access-Control-Allow-Origin': '*',
@@ -37,7 +36,7 @@ const AuthApiService = {
    postComment(username, text) {
     return fetch(`${config.API_ENDPOINT}/comments`, {
       method: 'POST',
-      mode: 'no-cors',
+      
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
@@ -56,7 +55,7 @@ const AuthApiService = {
   getComments(){
     return fetch(`${config.API_ENDPOINT}/comments`, {
       method: 'GET',
-      mode: 'no-cors',
+      
       headers: {
         'content-type': 'application/json',
         'Access-Control-Allow-Origin': '*',
